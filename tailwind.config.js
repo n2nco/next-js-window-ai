42c2bcf8361a708c5c44ddded8be4b6f3c62afec
B <36323943+n2nco@users.noreply.github.com>
2023-04-11 12:50:49 -0700

    minor ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,14 @@
 module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}",
   "./components/**/*.{js,ts,jsx,tsx}",
+  "./public/**/*.html"
 ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require("flowbite/plugin")
+  ],
 }
+
 
